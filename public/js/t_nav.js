c44ca924ef550cebd86e336f41980b01338b21cf
sunyiyu <853522929@qq.com>
2017-09-26 14:01:07 +0800

    息网公用头

---
+++ public/js/t_nav.js
@@ -0,0 +1,41 @@
+/**
+ * Created by Administrator on 2017/8/29.
+ */
+// JavaScript Document
+document.write('<div class="t_nav">')
+document.write('<div class="w">');
+document.write('<ul class="ul_left">');
+document.write('<li><a href="http://www.cnce.com" target="_blank">CNCE官网</a></li>');
+document.write('<li><a href="http://www.cottoneasy.com" target="_blank">e棉网</a></li>');
+document.write('<li><a href="http://www.i-cotton.org" target="_blank">i棉网</a></li>');
+document.write('<li><a href="http://www.cncexj.com" target="_blank">新疆监管棉花网</a></li>');
+document.write('</ul>');
+document.write('<ul class="ul_right">');
+document.write('<li><a href="javascript:;">注册</a></li>');
+document.write('<li><a href="javascript:;">登陆</a></li>');
+document.write('<li><a href="javascript:;"><i class="weibo"></i></a></li>');
+document.write('<li><a href="javascript:;"><i class="weixin"></i></a></li>');
+document.write('<li><a href="javascript:;">升级网员</a></li>');
+document.write('<li><a href="javascript:;">网站地图</a></li>');
+document.write('</ul>');
+document.write('</div>');
+document.write('</div>');
+document.write('<div class="b_nav">');
+document.write('<div class="w">');
+document.write('<ul class="cf">');
+document.write('<li><a href="http://www.cottonchina.org">首页<span>　|</span></a></li>');
+document.write('<li><a href="http://www.cottonchina.org/windows/window_new.php" target="_blank">更新列表<span>　|</span></a></li>');
+document.write('<li><a href="http://www.cottonchina.org/civil/index.php" target="_blank">国内<span>　|</span></a></li>');
+document.write('<li><a href="http://www.cottonchina.org/foreign/index.php" target="_blank">国际<span>　|</span></a></li>');
+document.write('<li><a href="http://www.cottonchina.org/textile/index.php" target="_blank">纺织<span>　|</span></a></li>');
+document.write('<li><a href="http://www.cottonchina.org/xinjiang/index.php" target="_blank">新疆<span>　|</span></a></li>');
+document.write('<li><a href="http://www.cottonchina.org/mianfu/index.php" target="_blank">棉副<span>　|</span></a></li>');
+document.write('<li><a href="http://www.cottonchina.org/futures/index.php" target="_blank">期货<span>　|</span></a></li>');
+document.write('<li><a href="http://www.cottonchina.org/newprice/index.php" target="_blank">价格<span>　|</span></a></li>');
+document.write('<li><a href="http://www.cottonchina.org/stat/index.php" target="_blank">统计<span>　|</span></a></li>');
+document.write('<li><a href="http://www.cottonchina.org/cotmov/index.php" target="_blank">视频<span>　|</span></a></li>');
+document.write('<li><a href="http://www.cottonchina.org/bridge-new2/index.php" target="_blank">商桥<span>　|</span></a></li>');
+document.write('<li><a href="http://www.cottonchina.org/english/index.php" target="_blank">Engish</a></li>');
+document.write('</ul>');
+document.write('</div>');
+document.write('</div>');
